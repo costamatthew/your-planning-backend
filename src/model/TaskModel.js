1633e83eb56a1524eee5a5d2a1f5171f184290c6
Mateus Costa <mateusdsilvacosta@outlook.com>
2021-04-21 21:20:13 -0300

    Connecting with MongoDB, and creating TaskModel

---
+++ src/model/TaskModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("../config/database");
+const Schema = mongoose.Schema;
+
+const TaskSchema = new Schema({
+  macaddress: { type: String, require: true },
+  type: { type: Number, require: true },
+  tittle: { type: String, require: true },
+  description: { type: String, require: true },
+  when: { type: Date, require: true },
+  done: { type: Boolean, default: false },
+  created: { type: Date, default: Date.now() },
+});
+
+module.exports = mongoose.model("Task", TaskSchema);
