a893156ecb953676fe1bb1b19ec3fdd23351725d
Mateus Costa <mateusdsilvacosta@outlook.com>
2021-07-03 01:52:47 -0300

    Add mongoose

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,20 @@ const mongoose = require("mongoose");
 
 const url = "mongodb://localhost:27017/yourplanning";
 
-mongoose.connect(url, { useNewUrlParser: true });
+//mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose
+  .connect("mongodb://127.0.0.1:27017/SUCCESS", {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    () => {
+      console.log("Database Successfully Connected");
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
 
 module.exports = mongoose;
